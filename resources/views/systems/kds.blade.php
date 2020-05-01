17fcd1781c529f1b3a939fee618490fdeb604d0c
Leonardo <leonardo.santos@foton.la>
2020-05-01 12:42:03 -0300

    first upload

---
+++ resources/views/systems/kds.blade.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Kitchen Display System</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vesperr - v2.0.0
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+  <main id="main">
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>SOBRE O KDS</h2>
+        </div>
+
+        <div class="row content">
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="150">
+            <p>
+              KDS é a abreviação de Kitchen Display System (Sistema de Tela de Cozinha). Ele nos permite:
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i> Integrar os pedidos com sistemas como Food, PDV e PRO.</li>
+              <li><i class="ri-check-double-line"></i> Acompanhar os Status dos Pedidos: Em Espera, Em Preparo, Em Pausa e Finalizados.</li>
+              <li><i class="ri-check-double-line"></i> Dar maior controle a produção de pedidos.</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts">
+      <div class="container">
+
+        <div class="row">
+          <div class="image col-xl-5 d-flex align-items-stretch justify-content-center justify-content-xl-start" data-aos="fade-right" data-aos-delay="150">
+            <img src="assets/img/counts-img.svg" alt="" class="img-fluid">
+          </div>
+
+          <div class="col-xl-7 d-flex align-items-stretch pt-4 pt-xl-0" data-aos="fade-left" data-aos-delay="300">
+            <div class="content d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-md-6 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-simple-smile"></i>
+                    <p><strong>Clientes</strong> estão felizes e satisfeitos utilizando nossa ferramenta.</p>
+                  </div>
+                </div>
+
+                <div class="col-md-6 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-document-folder"></i>
+                    <p><strong>O Projeto</strong> foi bem estruturado e o sistema sempre está disponível e funcional.</p>
+                  </div>
+                </div>
+
+                <div class="col-md-6 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-clock-time"></i>
+                    <p><strong>A Produtividade</strong> dos clientes tem aumentado e o KDS é um dos responsáveis.</p>
+                  </div>
+                </div>
+              </div>
+            </div><!-- End .content-->
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Implantação</h2>
+          <p>Sigam os passos abaixos para implantar o KDS em um cliente</p>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <h4 class="title"><a href="">Executar realease</a></h4>
+              <p class="description">Na pasta oficial do sistema no Google Drive, certifique-se de executar no banco de dados sempre a última release disponível. 
+                Não é necessário executar mais de uma realease.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+              <h4 class="title"><a href="">Atualizar o GWSERVICOS</a></h4>
+              <p class="description">Certifique-se de que o cliente está com a versão <bold>1.21.57</bold> ou superior do GWSERVICOS, caso contrário, o aplicativo não funcionará corretamente.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+              <h4 class="title"><a href="">Verificar o Manual</a></h4>
+              <p class="description">Na pasta oficial do KDS no Google Drive existe um manual para realizar o link das imagens de origem da venda dentro dos pedidos. Fiquem atentos.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
+              <h4 class="title"><a href="https://play.google.com/store/apps/details?id=com.va.kds">Manter o Aplicativo Atualizado</a></h4>
+              <p class="description">É importante manter o aplicativo atualizado, para que não existam conflitos entre o Aplicativo e o GWSERVICOS.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Configurações de Sistema</h2>
+          <p>Veja a seguir como configurar corretamente o sistema</p>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <h4 class="title"><a href="">Autenticação</a></h4>
+              <p class="description">Usuários se autenticam no KDS a partir de uma senha. Para isso, entre em Central do Colaborador, no PDV, associe o colaborador a empresa indicada,
+                defina uma senha e na aba "Dados KDS" defina qual o Perfil do Colaborador e qual o seu Setor.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+              <h4 class="title"><a href="">Configurar Setor</a></h4>
+              <p class="description">Cada impressora dentro da Central de Impressoras representa um Setor. Para que um setor receba pedidos KDS clique em alterar. Na aba KDS marque gerar KDS como "sim" e 
+                informe os outros parâmetros.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+              <h4 class="title"><a href="">Configurar Empresa</a></h4>
+              <p class="description">As empresas podem ter opções diferentes de impressão. Entre na Central de Empresa, navegue até a aba Gourmet e no campo Impressão/KDS informe o tipo correto indicado pelo cliente.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
+              <h4 class="title"><a href="https://play.google.com/store/apps/details?id=com.va.kds">Múltiplos Setores</a></h4>
+              <p class="description">Alguns clientes vendem por mais de uma Empresa. Nesse caso, ele pode optar por receber os pedidos em uma senha vinculada a múltiplos setores. Para isso, o código do colaborador deve 
+                ser vinculado a mais de um setor na tabela 765. Veja com mais detalhes no manual.
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+
+    <!-- ======= F.A.Q Section ======= -->
+    <section id="faq" class="faq">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Perguntas mais frequentes</h2>
+        </div>
+
+        <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-lg-5">
+            <i class="ri-question-line"></i>
+            <h4>Pedido não chega, o que fazer?</h4>
+          </div>
+          <div class="col-lg-7">
+            <p>
+              Verifique se o setor recebe KDS. Verifique se os produtos do pedido estão vinculados ao setor. Simule uma venda com os dados do pedido e veja se é recebido pelo KDS. 
+              Identifique se outros pedidos não chegaram, ou se foi apenas um em específico.
+            </p>
+          </div>
+        </div><!-- End F.A.Q Item-->
+
+        <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-5">
+            <i class="ri-question-line"></i>
+            <h4>Pedido duplicado</h4>
+          </div>
+          <div class="col-lg-7">
+            <p>
+              Ao realizar uma venda o cliente pode enviar o pedido para o KDS de duas maneiras: clicando no botão do PDV de enviar para produção e ao fechar a venda. 
+              Fazendo os dois ao mesmo tempo duplicará o pedido. Ceritifque-se também de ter executado a última versão da realease do KDS.
+            </p>
+          </div>
+        </div><!-- End F.A.Q Item-->
+
+
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>FEEDBACKS</h2>
+          <p>Veja o que os nossos clientes pensam sobre o KDS</p>
+        </div>
+
+        <div class="owl-carousel testimonials-carousel" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="testimonial-wrap">
+            <div class="testimonial-item">
+              <img src="" class="testimonial-img" alt="">
+              <h3>Dhouglas</h3>
+              <h4>Gerente, Páprica</h4>
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+        <div class="col-lg-6 text-lg-left text-center">
+          <div class="copyright">
+            &copy; Copyright <strong>Leonardo Santos</strong>. All Rights Reserved
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
